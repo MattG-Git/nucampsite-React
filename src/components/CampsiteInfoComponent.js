16a2f307592c6f0f3403e014d813c9ab61274873
Matt Goldhammer <Matt.Goldhammer@gmail.com>
2020-10-20 19:00:07 -0700

    Presentational and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -41,18 +41,18 @@ class CampsiteInfo extends Component {
     render() {
         if (this.props.campsite) {
             return (
-                <div className='row'>
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
                 </div>
             );
-        } else {
-            return (
-                <div></div>
-            );
         }
+        return <div />;
     }
 }
+
 
 
 export default CampsiteInfo;
