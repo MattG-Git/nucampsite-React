7210029b3096e8e840e673906385fdcd03633502
Matt Goldhammer <Matt.Goldhammer@gmail.com>
2020-10-20 19:16:36 -0700

    Presentational and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class CampsiteInfo extends Component {
-    renderCampsite(campsite) {
+   function RenderCampsite({campsite}) {
         return (
             <div className='col-md-5 m-1'>
                 <Card>
@@ -16,7 +15,7 @@ class CampsiteInfo extends Component {
         );
     }
 
-    renderComments(comments) {
+    function RenderComments({comments}) {
         if (comments) {
             return (
                 <div className='col-md-5 m-1'>
@@ -38,20 +37,20 @@ class CampsiteInfo extends Component {
             return <div />
     }
 
-    render() {
-        if (this.props.campsite) {
+    function CampsiteInfo(props) {
+        if (props.campsite) {
             return (
                 <div className="container">
                     <div className="row">
-                        {this.renderCampsite(this.props.campsite)}
-                        {this.renderComments(this.props.campsite.comments)}
+                        <RenderCampsite campsite ={props.campsite}/>
+                        <RenderComments comments= {props.campsite.comments}/>
                     </div>
                 </div>
             );
         }
         return <div />;
     }
-}
+
 
 
 
