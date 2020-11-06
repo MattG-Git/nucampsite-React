5a74e704c970204b99063ac074246dd472f4e12f
Matt Goldhammer <Matt.Goldhammer@gmail.com>
2020-11-05 20:46:22 -0800

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -23,10 +23,10 @@ class CommentForm extends Component {
         });
     }
     handleSubmit(values) {
-        console.log("Current state is: " + JSON.stringify(values));
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
         this.toggleModal();
-    }  
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
+    }
+    
     render() { 
         return ( 
             <div>
@@ -107,7 +107,7 @@ function RenderCampsite({campsite}) {
             </div>
         );
     }
-    function RenderComments({comments, addComment, campsiteId}) {
+    function RenderComments({comments, postComment, campsiteId}) {
         if (comments) {
             return (
                 <div className='col-md-5 m-1'>
@@ -123,7 +123,7 @@ function RenderCampsite({campsite}) {
                             </div>
                         );
                     })}
-                   <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                   <CommentForm campsiteId={campsiteId} postComment={postComment} />
                 </div>
             );
         }
@@ -165,11 +165,11 @@ function RenderCampsite({campsite}) {
                     </div>
                     <div className="row">
                         <RenderCampsite campsite={props.campsite} />
-                        <RenderComments 
+                        <RenderComments
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
-                    />
+                        />  
                     </div>
                 </div>
             );
--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,7 +1,7 @@
 import * as ActionTypes from './ActionTypes';
 import { baseUrl } from '../shared/baseUrl';
 
-export const addComment = (campsiteId, rating, author, text) => ({
+/*export const addComment = (campsiteId, rating, author, text) => ({
     type: ActionTypes.ADD_COMMENT,
     payload: {
         campsiteId: campsiteId,
@@ -9,7 +9,7 @@ export const addComment = (campsiteId, rating, author, text) => ({
         author: author,
         text: text
     }
-});
+});*/
 
 export const fetchCampsites = () => dispatch => {
     dispatch(campsitesLoading());
@@ -65,7 +65,7 @@ export const fetchComments = () => dispatch => {
             }
         )
         .then(response => response.json())
-        .then(comments => dispatch(addComments(comments)))
+        .then(comments => dispatch(addComment(comments)))
         .catch(error => dispatch(commentsFailed(error.message)));
 };
 
@@ -74,10 +74,47 @@ export const commentsFailed = errMess => ({
     payload: errMess
 });
 
-export const addComments = comments => ({
-    type: ActionTypes.ADD_COMMENTS,
-    payload: comments
+export const addComment = comment => ({
+    type: ActionTypes.ADD_COMMENT,
+    payload: comment
 });
+
+export const postComment = (campsiteId, rating, author, text) => dispatch => {
+    
+    const newComment = {
+        campsiteId: campsiteId,
+        rating: rating,
+        author: author,
+        text: text
+    };
+    newComment.date = new Date().toISOString();
+
+    return fetch(baseUrl + 'comments', {
+            method: "POST",
+            body: JSON.stringify(newComment),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => {
+                if (response.ok) {
+                    return response;
+                } else {
+                    const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                    error.response = response;
+                    throw error;
+                }
+            },
+            error => { throw error; }
+        )
+        .then(response => response.json())
+        .then(response => dispatch(addComment(response)))
+        .catch(error => {
+            console.log('post comment', error.message);
+            alert('Your comment could not be posted\nError: ' + error.message);
+        });
+};
+
 
 export const fetchPromotions = () => dispatch => {
     dispatch(promotionsLoading());
